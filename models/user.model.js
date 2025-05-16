fbe6d7223452f851a16ec447450d8e84f1980b85
Senevirathna12 <139707941+Senevirathna12@users.noreply.github.com>
2025-05-17 01:18:20 +0530

    user model

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstname : {
+        type :String,
+        required :[true, "Please provide firstname.."]
+    },
+    lastname : {
+        type :String,
+        required :[true, "Please provide lastname.."]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    gitAccessTiken:{
+        type : String
+    }
+})
