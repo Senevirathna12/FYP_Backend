e6dca27de521ca25db1608cd8ad3d5c97614544f
Senevirathna12 <139707941+Senevirathna12@users.noreply.github.com>
2025-05-23 11:45:55 +0530

    Project move to common repo for FYP

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,51 +1,62 @@
-const passport = require('passport');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const handleLogin = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
 
-const handleLogin = (req, res , next) => {
-    passport.authenticate("local", (err , user , info) => {
-        if(err){
-            return next(err)
-        }
+    if (!user) {
+      return res.status(200).json({
+        isSuccess: false,
+        message: info.message,
+      });
+    }
 
-        if(!user){
-            return res.status(200).json({
-                isSuccess : false,
-                message : info.message
-            })
-        }
+    req.login(user, async (err) => {
+      if (err) {
+        return next(err);
+      }
 
-        req.login(user, async (err) => {
-            if(err){
-                return next(err);
-            }
+      try {
+        const userId = { id: user._id };
+        const accessToken = jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "24",
+        });
 
-            try {
+        return res.status(200).json({
+          isSuccess: true,
+          message: "Logged in Successfully!",
+          accessToken: accessToken,
+          username: user.firstname,
+        });
+      } catch (err) {
+        console.error("Error generating JWT :", err);
+        return res.status(500).json({
+          isSuccess: false,
+          message: "Error with generating token.",
+        });
+      }
+    });
+  })(req, res, next);
+};
 
-                const userId = {id : user._id};
-                const accessToken = jwt.sign(userId , process.env.ACCESS_TOKEN_SECRET , {
-                    expiresIn : '24'
-                });
+const handleForgotPasswordPostRequest = async (req, res) => {
+  try {
 
-                return res.status(200).json({
-                    isSuccess : true,
-                    message : 'Logged in Successfully!',
-                    accessToken : accessToken,
-                    username : user.firstname
-                })
-                
-            } catch (err) {
-                console.error('Error generating JWT :', err);
-                return res.status(200).json({
-                    isSuccess : false,
-                    message : "Error with generating token."
-                })
-            }
-        })
-    })(req, res, next);
-}
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      isSuccess: false,
+      message: "Internal server error",
+      content: null,
+    });
+  }
+};
 
 module.exports = {
-    handleLogin,
-}
+  handleLogin,
+};
